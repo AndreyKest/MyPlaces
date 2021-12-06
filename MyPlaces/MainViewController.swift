76e4aea31246662dd1925e98638726176052139f
AlinaAndrey <fnona22@gmail.com>
2021-12-06 14:12:58 +0200

    segmented controller, change UITableViewController on UIViewController

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 import RealmSwift
 
-class MainViewController: UITableViewController {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    @IBOutlet var tableView: UITableView!
     var places: Results<Place>!
 
     override func viewDidLoad() {
@@ -20,11 +21,11 @@ class MainViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return places.isEmpty ? 0 : places.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         
 //        var content = cell.defaultContentConfiguration()
@@ -47,7 +48,7 @@ class MainViewController: UITableViewController {
     }
     
     //MARK: Table view delegate
-    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
         
         let place = places[indexPath.row]
         let deleteAction = UITableViewRowAction(style: .default, title: "Delete") { _, _ in
@@ -57,6 +58,7 @@ class MainViewController: UITableViewController {
         
         return [deleteAction]
     }
+    
 
     // MARK: - Navigation
 
